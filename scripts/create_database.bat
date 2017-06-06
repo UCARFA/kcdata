4bb655be821abe67d7da72c1756625e32afb1943
Steve Waltman <stevew@ucar.edu>
2017-06-06 15:06:01 -0600

    sjw

--- scripts/create_database.bat
+++ scripts/create_database.bat
@@ -1,3 +1,2 @@
 echo "This is the create_database.bat script"
-
-mysql.exe -hkcnonprod1 -P13306 -uroot -pturnacold < configuration\test.sql
+C:\mysql-5.6.28-win32\bin\mysql.exe -hkcnonprod1 -P13306 -uroot -pturnacold < configuration\test.sql
