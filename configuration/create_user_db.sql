fa19a2b531aeb2cd58be50856c0c1fd3c1e06408
Steve Waltman <stevew@ucar.edu>
2017-06-07 10:04:09 -0600

    SJW

---
+++ configuration/create_user_db.sql
@@ -0,0 +1,9 @@
+# DROP Existing database, and recreate
+DROP DATABASE IF EXISTS `coeus`;
+CREATE DATABASE `coeus` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
+
+# Grant privileges to the 'coeus' account.  This will create the account if it does not exist.
+# MySQL 5.7 introced 'CREATE USER IF NOT EXISTS', but we will have to do it this way until we
+# upgrade. 
+GRANT ALL PRIVILEGES ON `coeus`.* TO 'coeus'@'localhost' IDENTIFIED BY 'kuali' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON `coeus`.* TO 'coeus'@'%' IDENTIFIED BY 'kuali' WITH GRANT OPTION;
