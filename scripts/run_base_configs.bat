74c98144ae5050f97ddbb74ee7b60d4a86ac75b4
Steve Waltman <stevew@ucar.edu>
2017-06-07 15:22:12 -0600

    SJW

--- scripts/run_base_configs.bat
+++ scripts/run_base_configs.bat
@@ -1,3 +1,6 @@
 echo "Running run_configs.bat..."
 C:\mysql-5.6.28-win32\bin\mysql.exe -hkcnonprod1 -P13306 -uroot -pturnacold coeus < scripts\sql\insert_unit_hierarchy.sql
+C:\mysql-5.6.28-win32\bin\mysql.exe -hkcnonprod1 -P13306 -uroot -pturnacold coeus < scripts\sql\update_country_codes.sql
+C:\mysql-5.6.28-win32\bin\mysql.exe -hkcnonprod1 -P13306 -uroot -pturnacold coeus < scripts\sql\parameter_mods.sql
+C:\mysql-5.6.28-win32\bin\mysql.exe -hkcnonprod1 -P13306 -uroot -pturnacold coeus < scripts\sql\test_email_parameter_mods.sql
 C:\mysql-5.6.28-win32\bin\mysql.exe -hkcnonprod1 -P13306 -uroot -pturnacold coeus < scripts\sql\modify_sponsor_types.sql
---
+++ scripts/sql/test_email_parameter_mods.sql
@@ -0,0 +1,14 @@
+-- Enable and configure test emails
+-- --------------------------------
+
+UPDATE krcr_parm_t SET VAL = 'kctesters@ucar.edu' WHERE PARM_NM = "EMAIL_NOTIFICATION_FROM_ADDRESS"
+AND NMSPC_CD = "KC-GEN";
+
+UPDATE krcr_parm_t SET VAL = 'kctesters@ucar.edu' WHERE PARM_NM = "EMAIL_NOTIFICATION_TEST_ADDRESS"
+AND NMSPC_CD = "KC-GEN";
+
+UPDATE krcr_parm_t SET VAL = 'Y' WHERE PARM_NM = "EMAIL_NOTIFICATION_TEST_ENABLED"
+AND NMSPC_CD = "KC-GEN";
+
+UPDATE krcr_parm_t SET VAL = 'kctesters@ucar.edu' WHERE PARM_NM = "EMAIL_NOTIFICATION_TEST_ADDRESS"
+AND NMSPC_CD = "KR-WKFLW";
