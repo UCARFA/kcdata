f4c1c0cf9cc5d0bf91537c0368ad4793498d2098
Steve Waltman <stevew@ucar.edu>
2017-06-07 10:13:58 -0600

    SJW

--- scripts/create_database.bat
+++ scripts/create_database.bat
@@ -1,5 +1,5 @@
 echo "Running create_database.bat..."
 echo "    Create user, empty database, and grant privileges..."
-C:\mysql-5.6.28-win32\bin\mysql.exe -hkcnonprod1 -P13306 -uroot -pturnacold < configuration\create_user_db.sql
+C:\mysql-5.6.28-win32\bin\mysql.exe -hkcnonprod1 -P13306 -uroot -pturnacold < scripts\sql\create_user_db.sql
 echo "    Restore base 'coeus' database..."
 C:\mysql-5.6.28-win32\bin\mysql.exe -hkcnonprod1 -P13306 -uroot -pturnacold coeus < databases\coeus_base.sql
