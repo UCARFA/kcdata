de04eb96cdeee03b289a756217f6872e4cfe1d23
Steve Waltman <stevew@ucar.edu>
2017-06-07 10:29:08 -0600

    SJW

--- scripts/run_configs.bat
+++ scripts/run_configs.bat
@@ -1,3 +1,3 @@
 echo "Running run_configs.bat..."
-C:\mysql-5.6.28-win32\bin\mysql.exe -hkcnonprod1 -P13306 -uroot -pturnacold < scripts\sql\insert_unit_hierarchy.sql
-C:\mysql-5.6.28-win32\bin\mysql.exe -hkcnonprod1 -P13306 -uroot -pturnacold < scripts\sql\modify_sponsor_types.sql
+C:\mysql-5.6.28-win32\bin\mysql.exe -hkcnonprod1 -P13306 -uroot -pturnacold coeus < scripts\sql\insert_unit_hierarchy.sql
+C:\mysql-5.6.28-win32\bin\mysql.exe -hkcnonprod1 -P13306 -uroot -pturnacold coeus < scripts\sql\modify_sponsor_types.sql
