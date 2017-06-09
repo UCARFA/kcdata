cbec5f9044b1913ad056aa7da327ec35dde20770
Steve Waltman <stevew@ucar.edu>
2017-06-09 11:59:57 -0600

    sjw

---
+++ scripts/sql/Update_Parameter_Table.sql
@@ -0,0 +1,16 @@
+-- Disable Credit Splits (FAPA-413)
+-- --------------------------------
+
+START TRANSACTION;
+
+UPDATE krcr_parm_t SET VAL = 'N' WHERE PARM_NM = "award.creditsplit.enabled";
+
+UPDATE krcr_parm_t SET VAL = 'N' WHERE PARM_NM = "institutionalproposal.creditsplit.enabled";
+
+UPDATE krcr_parm_t SET VAL = 'N' WHERE PARM_NM = "proposaldevelopment.creditsplit.enabled";
+
+-- Disable PI Certification for IP Awards (FAPA-474)
+
+UPDATE krcr_parm_t SET VAL = '0' WHERE PARM_NM = "awardUncertifiedKeyPersonnel";
+
+COMMIT;
