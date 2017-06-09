1f7d664e7e0da57cc1f10427555cda977e4bc181
Steve Waltman <stevew@ucar.edu>
2017-06-09 12:45:18 -0600

    sjw

--- scripts/run_base_configs.bat
+++ scripts/run_base_configs.bat
@@ -1,2 +1,2 @@
 echo "Running run_base_configs.bat to server %1 port %2..."
-C:\mysql-5.6.28-win32\bin\mysql.exe -h%1 -P%2 -uroot -pturnacold coeus < .\scripts\sql\ucar_kc_configs_win.sql
+C:\mysql-5.6.28-win32\bin\mysql.exe -h%1 -P%2 -uroot -pturnacold coeus < .\scripts\sql\ucar_kc_configs_test.sql
