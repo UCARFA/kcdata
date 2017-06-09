2668bd1c4170467e32d1918dc289daf4ecf3af16
Steve Waltman <stevew@ucar.edu>
2017-06-09 14:49:12 -0600

    sjw

--- scripts/run_base_configs.sh
+++ scripts/run_base_configs.sh
@@ -1,2 +1,2 @@
 echo "Running run_base_configs.sh to server $1 port $2..."
-mysql -h$1 -P$2 -uroot -pturnacold coeus < ./scripts/sql/ucar_kc_configs.sql
+mysql -h$1 -P$2 -uroot -pturnacold coeus < ./scripts/sql/ucar_kc_configs_lnx.sql
--- scripts/sql/ucar_kc_configs.sql
+++ scripts/sql/ucar_kc_configs.sql
@@ -1,46 +1,46 @@
 -- FAPA 438 & 566
-\. ./scripts/sql/Update_Sponsor_Type.sql
+source scripts\sql\Update_Sponsor_Type.sql
 
 -- FAPA 413
-\. ./scripts/sql/Update_Parameter_Table.sql
+\. .\scripts\sql\Update_Parameter_Table.sql
 
 -- FAPA 469
-\. ./scripts/sql/Update_Country_Codes.sql
+\. .\scripts\sql\Update_Country_Codes.sql
 
 FAPA 711
-\. ./scripts/sql/Test_Emails.sql
+\. .\scripts\sql\Test_Emails.sql
 
 -- FAPA 711
-\. ./scripts/sql/Unit_Hierarchy.sql
+\. .\scripts\sql\Unit_Hierarchy.sql
 
 -- FAPA 520
-\. ./scripts/sql/Insert_Sponsor_Term.sql
+\. .\scripts\sql\Insert_Sponsor_Term.sql
 
 -- FAPA 375
-\. ./scripts/sql/Insert_Award_Status.sql
+\. .\scripts\sql\Insert_Award_Status.sql
 
 -- FAPA 388
-\. ./scripts/sql/Insert_award_attachment_type.sql
+\. .\scripts\sql\Insert_award_attachment_type.sql
 
 -- FAPA 478
-\. ./scripts/sql/Update_Contact_Type.sql
+\. .\scripts\sql\Update_Contact_Type.sql
 
 -- FAPA 641
-\. ./scripts/sql/Insert_Comment_Type.sql
+\. .\scripts\sql\Insert_Comment_Type.sql
 
 -- FAPA 642
-\. ./scripts/sql/Insert_Special_Review.sql
+\. .\scripts\sql\Insert_Special_Review.sql
 
 -- FAPA 643
-\. ./scripts/sql/Insert_Cost_Share_Type.sql
+\. .\scripts\sql\Insert_Cost_Share_Type.sql
 
 -- FAPA 658
-\. ./scripts/sql/Insert_NSF_Codes.sql
+\. .\scripts\sql\Insert_NSF_Codes.sql
 
 -- FAPA 450
-\. ./scripts/sql/Organization_Type_List.sql
+\. .\scripts\sql\Organization_Type_List.sql
 
 -- FAPA 764
--- \. ./scripts/sql/Award_Transaction_Type.sql
+\. .\scripts\sql\Award_Transaction_Type.sql
 
 COMMIT;
