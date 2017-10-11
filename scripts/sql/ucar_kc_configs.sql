cc5ad72cd5d00b5bc09b75cdb4197fdcaf15d8a9
heidifrisch <hfrisch@ucar.edu>
2017-10-11 11:10:10 -0600

    Add files via upload

--- scripts/sql/ucar_kc_configs.sql
+++ scripts/sql/ucar_kc_configs.sql
@@ -64,4 +64,23 @@
 -- FAPA 1436
 \. .\scripts\sql\Update_Notification_Type.sql
 
+-- FAPA 1454;
+\. .\scripts\sql\Insert_Reports_Status.sql
+
+-- FAPA 977
+\. .\scripts\sql\Insert_CFDA_Values.sql
+
+-- FAPA 1470
+\. .\scripts\sql\Provision_Permissions.sql
+
+-- FAPA 1433
+\. .\scripts\sql\Insert_Award_Closeout.sql
+
+-- FAPA 522
+\. .\scripts\sql\Update_Organization.sql
+
+-- FAPA 1431
+\. .\scripts\sql\Insert_Individual_Units.sql
+
+
 COMMIT;
