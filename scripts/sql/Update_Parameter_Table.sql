13f7548bd4e0896b9d562e336baec3aae7d10f16
Steve Waltman <stevew@ucar.edu>
2018-01-25 11:14:57 -0700

    Added parameter update to increase limit on file attachment size to 25M

--- scripts/sql/Update_Parameter_Table.sql
+++ scripts/sql/Update_Parameter_Table.sql
@@ -43,9 +43,9 @@ UPDATE krcr_parm_t SET VAL = 'admin' WHERE PARM_NM = 'CFDA_BATCH_NOTIFICATION_RE
 
 -- Configure Max File Size Parameter (FAPA-2040)
 
--- UPDATE krcr_parm_t SET VAL = '25M' WHERE PARM_NM = 'MAX_FILE_SIZE_ATTACHMENT';
+UPDATE krcr_parm_t SET VAL = '25M' WHERE PARM_NM = 'MAX_FILE_SIZE_ATTACHMENT';
 
--- UPDATE krcr_parm_t SET VAL = '25M' WHERE PARM_NM = 'MAX_FILE_SIZE_DEFAULT_UPLOAD';
+UPDATE krcr_parm_t SET VAL = '25M' WHERE PARM_NM = 'MAX_FILE_SIZE_DEFAULT_UPLOAD';
 
 -- Set Fiscal Start Month Parameter to 10 (FAPA-2148)
 
