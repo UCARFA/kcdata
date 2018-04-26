5fd205e7b7b4355ff5bca9e64e3358d1da7bc4ae
Steve Waltman <stevew@ucar.edu>
2018-04-26 15:49:15 -0600

    test

--- scripts/sql/testTeamCityLoad.sql
+++ scripts/sql/testTeamCityLoad.sql
@@ -7,7 +7,7 @@ CREATE TEMPORARY TABLE `tmp_testCSVLoad` (
 `new_obl_start_date` varchar(15) collate utf8_bin NOT NULL DEFAULT ''
 );
 
-LOAD DATA LOCAL INFILE 'scripts\\sql\\dataFiles\\testLoad.csv'
+LOAD DATA LOCAL INFILE 'scripts\\sql\\dataFiles\\testLoad2.csv'
 -- LOAD DATA LOCAL INFILE '~/git/kcdata/scripts/sql/dataFiles/testLoad.csv'
 INTO TABLE tmp_testCSVLoad
 FIELDS TERMINATED BY ','
